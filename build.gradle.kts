547aa827e82cba293afeeffa662aed64cdd053bd
Rahul <137375203+therahul-yo@users.noreply.github.com>
2024-11-05 18:14:55 +0530

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+import com.android.build.gradle.BaseExtension
+import com.android.build.gradle.BasePlugin
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.android.library) apply false
+  alias(libs.plugins.kotlin.android) apply false
+  alias(libs.plugins.kotlin.parcelize) apply false
+  alias(libs.plugins.kotlin.dokka) apply false
+  alias(libs.plugins.detekt) apply false
+}
+
+subprojects {
+  tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions {
+      allWarningsAsErrors = true
+      progressiveMode = true
+      jvmTarget = JvmTarget.JVM_11
+    }
+    if ((this@subprojects.name != "sample")) {
+      explicitApiMode = ExplicitApiMode.Strict
+    }
+  }
+
+  plugins.withType<BasePlugin>().configureEach {
+    extensions.configure<BaseExtension> {
+      compileSdkVersion(libs.versions.androidconfig.compileSdk.get().toInt())
+      buildToolsVersion(libs.versions.androidconfig.buildTools.get())
+      defaultConfig {
+        minSdk = libs.versions.androidconfig.minSdk.get().toInt()
+        targetSdk = libs.versions.androidconfig.targetSdk.get().toInt()
+      }
+      compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+      }
+    }
+  }
+
+  apply(plugin = rootProject.libs.plugins.detekt.get().pluginId)
+  extensions.configure<DetektExtension> {
+    toolVersion = rootProject.libs.versions.detekt.get()
+    config.setFrom(files("$rootDir/detekt.yml"))
+    buildUponDefaultConfig = true
+    ignoredBuildTypes = listOf("release")
+  }
+  dependencies {
+    add("detektPlugins", rootProject.libs.detektFormatting)
+  }
+  tasks.withType<Detekt>().configureEach {
+    jvmTarget = JvmTarget.JVM_11.target
+  }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+include(":quickie", ":sample")
+
+dependencyResolutionManagement {
+  repositories {
+    google()
+    mavenCentral()
+  }
+}
+
+pluginManagement {
+  repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+  }
+}
